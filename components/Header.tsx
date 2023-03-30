20c2a7ede2ef87a79b36a7da9b45a54bf6b616f4
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-03-30 23:17:52 +0530

    created template

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <header>
+        <div>
+            
+        </div>
+    </header>
+  )
+}
+
+export default Header
