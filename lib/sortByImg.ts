a84a80832335ed26023a1ceedbc288712344c996
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-03-31 15:45:48 +0530

    why am i this dumb?

--- lib/sortByImg.ts
+++ lib/sortByImg.ts
@@ -1,4 +1,4 @@
-export default function sortByImg(news: NewsResponse[]) {
+export default function sortByImg(news: NewsResponse) {
     
   const newsWithImg = news.data.filter((item) => item.image !== null);
   const newsWithoutImg = news.data.filter((item) => item.image === null);
